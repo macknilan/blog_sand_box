47d9b8748398d202f6b4cd9b043378c03b84b198
Rodolfo <rodolfougaldeochoa@gmail.com>
2024-06-03 18:14:14 -0600

    :rocket: [MOD] value of the -DEBUG- variable

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,7 +4,8 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG")
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
